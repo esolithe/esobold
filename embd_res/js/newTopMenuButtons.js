1341e1f65431b4d0a20c2c66a362bfd67fbf5cfc
prima <prima@localhost>
2025-10-14 22:50:40 +0100

    feat: Added hover button to save directly

--- embd_res/js/newTopMenuButtons.js
+++ embd_res/js/newTopMenuButtons.js
@@ -7,9 +7,30 @@ window.addEventListener('load', () => {
 		</li>`
     topButton += `<li class="nav-item" id="topbtn_data_manager">
 			<a class="nav-link mainnav" href="#" onclick="closeTopNav(); showCharacterList();" tabindex="0">Data</a>
+			<a id="topbtn_save_current" class="nav-link mainnav hidden" href="#" tabindex="0" style="position: absolute; margin-top: 10px;">Save</a>
 		</li>`
 
     document.querySelector("#navbarNavDropdown > ul").innerHTML += topButton;
     treeViewer.showOpenButton();
     updateLegacySaveButtonState();
+
+    let dataElem = document.querySelector("#topbtn_data_manager")
+    dataElem.addEventListener("mouseenter", () => {
+        document.querySelector("#topbtn_save_current").classList.remove("hidden")
+    })
+    dataElem.addEventListener("mouseleave", () => {
+        document.querySelector("#topbtn_save_current").classList.add("hidden")
+    })
+
+    document.querySelector("#topbtn_save_current").addEventListener("click", () => {
+        inputBox("Enter a Filename", "Save File", "", "Input Filename", () => {
+            let userinput = getInputBoxValue();
+            if (userinput != null && userinput.trim() != "") {
+                waitingToast.show()
+                waitingToast.setText(`Saving data ${userinput}`)
+                let data = generate_savefile(true, true, true);
+                saveKLiteSaveToIndexDB(userinput, data);
+            }
+        }, false);
+    })
 })
